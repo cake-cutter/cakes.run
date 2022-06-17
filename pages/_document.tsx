1f854d764f244d9378f229924b4de826cc99e203
kekda-py <yashverma30115@gmail.com>
2022-06-17 10:43:48 +0530

    add umami analytics

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script async defer data-website-id="349fb4cb-9bc2-460d-b915-51d2d173f860" src="https://umami.dhravya.dev/umami.js"></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
