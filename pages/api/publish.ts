09b90f297e8ca1ada36fd4913d814a8ae7516828
kekda-py <yashverma30115@gmail.com>
2022-06-17 18:59:34 +0530

    add token instead of name in the apo

--- pages/api/publish.ts
+++ pages/api/publish.ts
@@ -22,60 +22,89 @@ export default async function handler(
 ) {
 	if (req.method === "POST") {
 		try {
-			const body: Req = req.body;
 			
-			const data = await prisma.cake.findFirst({
-				where : {
-					name: {
-						equals : body.name
-					},
-					author : {
-						not : body.author
+			const body: Req = req.body;
+
+			const resp = await fetch(
+				`https://api.github.com/user`,
+				{
+					headers : {
+						"Authorization": `token ${body.author}`
 					}
-				},
-			})
+				}
+			)
+			const j = await resp.json();
+
+			if (j.login) {
+				const data = await prisma.cake.findFirst({
+					where : {
+						name: {
+							equals : body.name
+						},
+						author : {
+							not : j.login
+						}
+					},
+				})
+			
+				if (data) {
+					res.status(403).send({
+						success: 1,
+						error: "Cake already exists, and you don't own it... So you can't update it!"
+					})
+					return
+				}
 	
-			if (data) {
-				res.status(403).send({
+				if (body.name.includes(" ")) {
+					res.status(400).send({
+						success: 1,
+						error: "Name can't contain spaces"
+					})
+					return
+				}
+	
+				if (body.name.length < 1) {
+					res.status(400).send({
+						success: 1,
+						error: "Name is required"
+					})
+					return
+				}
+	
+				await prisma.cake.upsert({
+					where : {
+						name : body.name
+					},
+					update : {
+						lastUpdate : new Date(),
+						name: body.name,
+						short: body.short,
+						dsc: body.dsc,
+						author: j.login,
+						cake: body.cake,
+					},
+					create : {
+						name: body.name,
+						short: body.short,
+						dsc: body.dsc,
+						author: j.login,
+						cake: body.cake,
+					}
+				})
+	
+				res.status(200).send({
+					success: 0,
+					data: "Cake updated!"
+				})
+	
+				return
+			} else {
+				res.status(400).json({
 					success: 1,
-					error: "Cake already exists, and you don't own it... So you can't update it!"
+					error: "Invalid token. This is probably because of the change in the api so please update the cli"
 				})
 				return
 			}
-
-			if (body.name.includes(" ")) {
-				res.status(400).send({
-					success: 1,
-					error: "Name can't contain spaces"
-				})
-				return
-			}
-
-			if (body.name.length < 1) {
-				res.status(400).send({
-					success: 1,
-					error: "Name is required"
-				})
-				return
-			}
-
-			await prisma.cake.upsert({
-				where : {
-					name : body.name
-				},
-				update : {
-					lastUpdate : new Date(),
-					...body
-				},
-				create : body
-			})
-
-			res.status(200).send({
-				success: 0,
-				data: "Cake updated!"
-			})
-
-			return
 
 
 		} catch (e) {
